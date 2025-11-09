8d271b9342d3e7866ecbab5ad8fa7c1cccb78910
mignaciourra2005 <mignaciourra2005@gmail.com>
2025-11-09 08:04:16 -0500

    Add files via upload

---
+++ src/main/java/com/espol/controlador/ControladorSesionEnfoque.java
@@ -0,0 +1,24 @@
+package com.espol.controlador;
+
+import com.espol.modelo.SesionEnfoque;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControladorSesionEnfoque {
+    private List<SesionEnfoque> sesiones;
+
+    public ControladorSesionEnfoque() {
+        this.sesiones = new ArrayList<>();
+    }
+
+    public void agregarSesion(String tecnica, int duracion) {
+        LocalDate fecha = LocalDate.now();
+        SesionEnfoque nuevaSesion = new SesionEnfoque(fecha, tecnica, duracion);
+        sesiones.add(nuevaSesion);
+    }
+
+    public List<SesionEnfoque> getSesiones() {
+        return sesiones;
+    }
+}
