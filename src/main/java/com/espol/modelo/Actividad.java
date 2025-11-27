41f0ce2fa32e9b1e29f7a131dacf562d827c7fa1
Misael Zambrano <217348810+misaelzb@users.noreply.github.com>
2025-11-26 19:38:10 -0500

    Identaciones

--- src/main/java/com/espol/modelo/Actividad.java
+++ src/main/java/com/espol/modelo/Actividad.java
@@ -1,84 +1,102 @@
 package com.espol.modelo;
-import java.time.LocalDateTime;
-public class Actividad {
-protected int id;
-protected String nombre;
-protected String descripcion;
-protected LocalDateTime fechaVencimiento;
-protected String prioridad;
-protected double tiempoEstimado;
-protected double avance;
-protected String tipo;
-protected static int idcrecimiento=5;
-//Por comodidad en un tiempo desarrollé 4 constructores, ademas de todos los atributos protegidos para poder trabajar mejor en la subclase Vista
-public Actividad(String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad, double tiempoEstimado, double avance, String tipo){
-    id=idcrecimiento++;
-    this.nombre=nombre;
-    this.descripcion=descripcion;
-    this.fechaVencimiento=fechaVencimiento;
-    this.prioridad=prioridad;
-    this.tiempoEstimado=tiempoEstimado;
-    this.avance=avance;
-    this.tipo=tipo;
-}
-public Actividad(int id, String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad, double tiempoEstimado, double avance, String tipo){
-    this.id=id;
-    this.nombre=nombre;
-    this.descripcion=descripcion;
-    this.fechaVencimiento=fechaVencimiento;
-    this.prioridad=prioridad;
-    this.tiempoEstimado=tiempoEstimado;
-    this.avance=avance;
-    this.tipo=tipo;
-}
-public Actividad(String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad, double tiempoEstimado, String tipo){
-    id=idcrecimiento++;
-    this.nombre=nombre;
-    this.descripcion=descripcion;
-    this.fechaVencimiento=fechaVencimiento;
-    this.prioridad=prioridad;
-    this.tiempoEstimado=tiempoEstimado;
-    avance=0.0;
-    this.tipo=tipo;
-}
-public Actividad(){
-    this.id=00;
-    this.nombre="a";
-    this.descripcion="a";
-    this.fechaVencimiento=LocalDateTime.now();
-    this.prioridad="a";
-    this.avance=0.0;
-    this.tipo="i";
-}
-//getters
-public int getId(){
-    return this.id;
-}
-public String getNombre(){
-    return this.nombre;
-}
-public String getDescripcion(){
-    return this.descripcion;
-}
-public LocalDateTime getFechavencimiento(){
-    return this.fechaVencimiento;
-}
-public String getPrioridad(){
-    return this.prioridad;
-}
-public double getTiempoEstimado(){
-    return this.tiempoEstimado;
-}
-public double getAvance(){
-    return this.avance;
-    
-}
-public String getTipo(){
-    return this.tipo;
-}
-//Setter
-public void setAvance(double avance){
-    this.avance=avance;
-}
-}
 
+import java.time.LocalDateTime;
+
+public class Actividad {
+    protected int id;
+    protected String nombre;
+    protected String descripcion;
+    protected LocalDateTime fechaVencimiento;
+    protected String prioridad;
+    protected double tiempoEstimado;
+    protected double avance;
+    protected String tipo;
+    protected static int idcrecimiento = 5;
+
+    // Por comodidad en un tiempo desarrollé 4 constructores, ademas de todos los
+    // atributos protegidos para poder trabajar mejor en la subclase Vista
+    public Actividad(String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad,
+            double tiempoEstimado, double avance, String tipo) {
+        id = idcrecimiento++;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaVencimiento = fechaVencimiento;
+        this.prioridad = prioridad;
+        this.tiempoEstimado = tiempoEstimado;
+        this.avance = avance;
+        this.tipo = tipo;
+    }
+
+    public Actividad(int id, String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad,
+            double tiempoEstimado, double avance, String tipo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaVencimiento = fechaVencimiento;
+        this.prioridad = prioridad;
+        this.tiempoEstimado = tiempoEstimado;
+        this.avance = avance;
+        this.tipo = tipo;
+    }
+
+    public Actividad(String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad,
+            double tiempoEstimado, String tipo) {
+        id = idcrecimiento++;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaVencimiento = fechaVencimiento;
+        this.prioridad = prioridad;
+        this.tiempoEstimado = tiempoEstimado;
+        avance = 0.0;
+        this.tipo = tipo;
+    }
+
+    public Actividad() {
+        this.id = 00;
+        this.nombre = "a";
+        this.descripcion = "a";
+        this.fechaVencimiento = LocalDateTime.now();
+        this.prioridad = "a";
+        this.avance = 0.0;
+        this.tipo = "i";
+    }
+
+    // getters
+    public int getId() {
+        return this.id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public LocalDateTime getFechavencimiento() {
+        return this.fechaVencimiento;
+    }
+
+    public String getPrioridad() {
+        return this.prioridad;
+    }
+
+    public double getTiempoEstimado() {
+        return this.tiempoEstimado;
+    }
+
+    public double getAvance() {
+        return this.avance;
+
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    // Setter
+    public void setAvance(double avance) {
+        this.avance = avance;
+    }
+}
--- src/main/java/com/espol/modelo/ActividadPersonal.java
+++ src/main/java/com/espol/modelo/ActividadPersonal.java
@@ -1,22 +1,30 @@
 package com.espol.modelo;
+
 import java.time.LocalDateTime;
-public class ActividadPersonal extends Actividad{
-protected String lugar;
-//Herencia donde se llaman 2 constructores diferentes, para poder colocar los valores iniciales.
-public ActividadPersonal( int id,String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad, double tiempoEstimado, double avance, String tipo, String lugar){
 
-super(id, nombre, descripcion, fechaVencimiento,prioridad, tiempoEstimado, avance, tipo );
+public class ActividadPersonal extends Actividad {
+    protected String lugar;
 
-this.lugar=lugar;
-}
-public ActividadPersonal(String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad, double tiempoEstimado, double avance, String tipo, String lugar){
+    // Herencia donde se llaman 2 constructores diferentes, para poder colocar los
+    // valores iniciales.
+    public ActividadPersonal(int id, String nombre, String descripcion, LocalDateTime fechaVencimiento,
+            String prioridad, double tiempoEstimado, double avance, String tipo, String lugar) {
 
-super(nombre, descripcion, fechaVencimiento,prioridad, tiempoEstimado, avance, tipo );
+        super(id, nombre, descripcion, fechaVencimiento, prioridad, tiempoEstimado, avance, tipo);
 
-this.lugar=lugar;
-}
-//Getters
-public String getLugar(){
-    return this.lugar;
-}
+        this.lugar = lugar;
+    }
+
+    public ActividadPersonal(String nombre, String descripcion, LocalDateTime fechaVencimiento, String prioridad,
+            double tiempoEstimado, double avance, String tipo, String lugar) {
+
+        super(nombre, descripcion, fechaVencimiento, prioridad, tiempoEstimado, avance, tipo);
+
+        this.lugar = lugar;
+    }
+
+    // Getters
+    public String getLugar() {
+        return this.lugar;
+    }
 }
